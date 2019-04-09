f2cf34e1aa28f6b46683a474b68740f9fffd0e8f
Giovanni Rago <giovanni.rago@outlook.com>
2019-04-09 15:04:43 +0200

    ...

--- src/test/java/AbstractTest.java
+++ src/test/java/AbstractTest.java
@@ -26,7 +26,7 @@ public class AbstractTest {
     private ThreadLocal<AppiumDriver> webDriver = new ThreadLocal<>();
     private ThreadLocal<String> sessionId = new ThreadLocal<>();
 
-    private String sauceURI = "@ondemand.saucelabs.com:443";
+    private String sauceURI = "@ondemand.eu-central-1.saucelabs.com";
     private String buildTag = System.getenv("BUILD_TAG");
     private String username = System.getenv("SAUCE_USERNAME");
     private String accesskey = System.getenv("SAUCE_ACCESS_KEY");
